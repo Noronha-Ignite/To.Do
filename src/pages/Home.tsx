8fe954a80450717b3b8662f8393945a54f505979
NoronhaGCP <inc.691@gmail.com>
2022-02-11 14:55:57 -0300

    Implemetação do desafio

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,27 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const task: Task = {
+      id: new Date().getTime(),
+      done: false,
+      title: newTaskTitle,
+    };
+
+    setTasks(previousTasks => [...previousTasks, task]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    setTasks(previousTasks => previousTasks.map(task => {
+      if (task.id === id) {
+        return { ...task, done: !task.done };
+      }
+
+      return task;
+    }));
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(previousTasks => previousTasks.filter(task => task.id !== id));
   }
 
   return (
